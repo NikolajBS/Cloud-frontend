189e8b1cd9a1383c6c49f4c9b7153ecba950b335
impostermp <math-351@hotmail.com>
2023-12-01 03:35:29 +0100

    small test

--- src/App.js
+++ src/App.js
@@ -3,40 +3,12 @@ import { useQuery, useMutation } from "react-query";
 import axios from "axios";
 
 function App() {
-  const backendAddress = process.env.REACT_APP_BACKEND_ADDRESS;
+  const backendAddress = process.env.REACT_APP_BACKEND_ADDRESS || "lol";
   console.log("Backend Address:", backendAddress);
-  const fetchMenu = async () => {
-    return await axios
-      .get(`${backendAddress}/menu`)
-      .then((response) => response.data);
-  };
-  const { data, error, isLoading } = useQuery("menu", fetchMenu);
-
-  const orderMutation = useMutation((order) =>
-    axios.post(`${backendAddress}/order`, order)
-  );
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error...</div>;
-  }
-
-  const handlePlaceOrder = (item) => {
-    orderMutation.mutate(item);
-  };
 
   return (
     <>
-      <h1>Menu</h1>
-      {data.map((food) => (
-        <li>
-          {food.name} - {food.price}
-          <button onClick={() => handlePlaceOrder(food)}>Add to cart</button>
-        </li>
-      ))}
+      <p>this is a test</p>
     </>
   );
 }
